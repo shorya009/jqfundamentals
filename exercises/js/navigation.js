d158c92f3a484c1e92d565c19d40367b0f772bfe
Shorya Gupta <61547437+shorya009@users.noreply.github.com>
2020-07-02 18:38:30 +0530

    Update navigation.js

---
+++ exercises/js/navigation.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+  $('#nav li')
+    .hover(function() {
+      $(this)
+        .addClass('hover')
+        .find('ul').show();
+    }, function() {
+      $(this)
+        .removeClass('hover')
+        .find('ul').hide();
+    });
+});
