560603a9d2f6c164382464d3d2462aff0173e1c3
Shorya Gupta <61547437+shorya009@users.noreply.github.com>
2020-07-08 19:41:16 +0530

    Update navigation.js

--- exercises/js/navigation.js
+++ exercises/js/navigation.js
@@ -1,12 +1,25 @@
-$(document).ready(function() {
-  const nav = $('#nav li');
-  nav.hover(
-    () => { //hover
-      $(this).addClass('hover').find('ul').show();
-    },
-    () => { //out
-      $(this).removeClass('hover').removeClass('hover')
-        .find('ul').hide();
+$(function() {
+  const $nav = $('#nav li'),
+  unorderedList = 'ul',
+  hoverClass = 'hover';
+
+  $nav
+    .mouseover(function() { //hover
+      $(this).addClass(hoverClass).find(unorderedList).show();
+    })
+    .mouseout(function(){ //out
+      $(this).removeClass(hoverClass).find(unorderedList).hide();
     }
   );
 });
+
+
+
+//Q1 Is there another method to specify $(document).ready?
+ans yes there are several ways one the way is $(function() {}
+                                                
+//when do we use => function
+ans we use it for one the main reasons when we want our "this" to be lexically binded and not by any other object that calls it                                             
+                                                
+  
+  
