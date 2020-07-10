5ca142c767acb1e479932c6a88dd12f6e83d9cd1
Shorya Gupta <61547437+shorya009@users.noreply.github.com>
2020-07-10 20:57:54 +0530

    Update load.js

--- exercises/js/load.js
+++ exercises/js/load.js
@@ -1,13 +1,31 @@
-let $h3 = $('#blog').find('h3');
+class LoadExternalContent {
+  constructor($h3) {
+    this.$h3 = $h3;
+  }
 
-$h3.each(function(index, elements) {
-  let h3 = $(elements);
-  $(`<div id= "post${index+1}"></div>`).appendTo(h3);
-  $(this).data(`key`, `post${index+1}`);
-});
+  loadContent(e) {
+    e.preventDefault();
+    let divId = $(this).data('key');
+    $(`#${divId}`).load(`data/blog.html #${divId}`);
+  }
 
-$h3.bind("click", function(e) {
-  e.preventDefault();
-  let divId = $(this).data('key');
-  $(`#${divId}`).load(`data/blog.html #${divId}`);
+  bindEvent() {
+    this.$h3.bind("click", this.loadContent);
+  }
+
+  createTargetDiv() {
+    this.$h3.each(function(index, elements) {
+      let h3 = $(elements);
+      $(`<div id= "post${index+1}"></div>`).appendTo(h3);
+      $(this).data(`key`, `post${index+1}`);
+    });
+    this.bindEvent();
+  }
+
+}
+
+$(function() {
+  let $h3 = $('.module').find('[data-name = "heading"]'),
+  loadExternalContent = new LoadExternalContent($h3);
+  loadExternalContent.createTargetDiv();
 });
