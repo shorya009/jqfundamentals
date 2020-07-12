47e92755ced6328e75f7deca75eb40d4c0cc38d0
Shorya Gupta <61547437+shorya009@users.noreply.github.com>
2020-07-12 14:51:45 +0530

    Update slideshow.js

--- exercises/js/slideshow.js
+++ exercises/js/slideshow.js
@@ -1,18 +1,36 @@
-let slides = $('#slideshow li');
-slides.hide();
-let slideCout = 0;
-let totalSlides = slides.length;
+class SlideShow {
 
-slideshow = () => {
+  constructor(slides, slideCount, totalSlides) {
+    this.slides = slides;
+    this.slideCount = slideCount;
+    this.totalSlides = totalSlides;
+  }
 
-  slides.eq(slideCout).fadeIn(1000).delay(1000).fadeOut(() => {
-    slideCout < totalSlides - 1 ? slideCout++ : slideCout = 0;
-    slideshow();
-  });
-};
+  slideShowCounter() {
+    this.slideCount < this.totalSlides - 1 ? this.slideCount++ : this.slideCount = 0;
+    $('#current-image').html(this.slideCount + 1);
+  }
 
-$(`<ul id="nav">
-<li>Total Images ${totalSlides}</li> 
-</ul>`).insertAfter('#slideshow');
+  startSlideShow() {
+    this.slides.eq(this.slideCount).fadeIn(1000).delay(1000).fadeOut(() => {
+      this.slideShowCounter();
+      this.startSlideShow();
+    });
+  }
 
-slideshow();
+  init() {
+    this.slides.hide();
+    this.startSlideShow();
+    $(`<div><span id = "current-image">1</span>
+      / <span id = "total-images">${this.totalSlides}</span>
+      </div>`).insertAfter('#slideshow');
+  }
+}
+
+$(function() {
+  let slides = $('#slideshow li'),
+  slideCount = 0,
+  totalSlides = slides.length;
+  slideShow = new SlideShow(slides, slideCount, totalSlides);
+  slideShow.init();
+});
